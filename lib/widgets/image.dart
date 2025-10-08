cdc3aad933cc73ee860ac688c2442d9c85946d5e
joeljhou <joeljhou336@gmail.com>
2025-10-09 01:03:07 +0800

    comment: Image BoxFit 模式说明

--- lib/widgets/image.dart
+++ lib/widgets/image.dart
@@ -12,7 +12,7 @@ class ImageDemo extends StatelessWidget {
         children: const <Widget>[
           // 本地图片
           Image(
-            image: AssetImage("images/flutter.png"),  // 从项目资源中加载图片
+            image: AssetImage("assets/images/flutter.png"),  // 从项目资源中加载图片
             width: 200.0,                             // 图片显示宽度
             height: 200.0,                            // 图片显示高度
             fit: BoxFit.contain,                      // 保持图片宽高比，完整显示图片
@@ -31,3 +31,12 @@ class ImageDemo extends StatelessWidget {
     );
   }
 }
+
+// BoxFit 模式说明：
+// fill：拉伸填充容器，会导致图片变形
+// contain（默认）：等比缩放（放大/缩小）适应容器，可能出现留白
+// cover：等比放大后，居中填满容器，超出部分会被裁剪
+// fitWidth：宽度填满容器，高度按比例缩放
+// fitHeight：高度填满容器，宽度按比例缩放
+// none：保持原始尺寸，图片比容器大时居中显示，多余部分裁剪
+// scaleDown：保持比例，如果图片大于容器则缩小适应，否则保持原始大小
